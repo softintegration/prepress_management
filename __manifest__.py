2501e714171164bbe7ec0845d3735a2c651b1c66
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2022-12-26 12:00:15 +0100

    [FIX] Fix the module version

--- __manifest__.py
+++ __manifest__.py
@@ -10,7 +10,7 @@
     'qweb': [],
     'description': False,
     'images': [],
-    'version': '1.0.1.22',
+    'version': '1.0.1.30',
     'category': 'Prepress',
     'demo': [],
     'depends': ['portal','cancel_motif','product_customer','web_custom_groups'],
