2f3bc0c5807d10f3e6ebbb3e097cbd467d994ffa
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2022-12-29 15:19:29 +0100

    Product:Add <Nomber of colors> Field

--- models/product_template.py
+++ models/product_template.py
@@ -10,6 +10,7 @@ class ProductTemplate(models.Model):
     prepress_type = fields.Many2one('prepress.type', string='Type')
     prepress_proof_next_version = fields.Integer(string='Prepress proof next version', readonly=True, default=0)
     prepress_proofs_count = fields.Integer(compute='_compute_prepress_proofs_count')
+    color_cpt = fields.Integer(string='Number of Colors')
 
 
     def _increment_prepress_proof_version(self):
