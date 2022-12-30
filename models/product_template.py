2166c85bbfefb1cb0e9d350bcefcd64c42e47de2
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2022-12-30 20:55:06 +0100

    Product:Add <Both sides> field

--- models/product_template.py
+++ models/product_template.py
@@ -13,6 +13,7 @@ class ProductTemplate(models.Model):
     color_cpt = fields.Integer(string='Number of Colors')
     with_braille = fields.Boolean(string='With braille')
     complexity_factor_id = fields.Many2one('complexity.factor', string="Complexity factor")
+    both_sides = fields.Boolean(string='Both sides', default=False)
 
 
     def _increment_prepress_proof_version(self):
