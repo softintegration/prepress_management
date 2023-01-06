019c2637d2261273c5cdaa94cc4b9ee745a43a3c
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2023-01-06 17:10:34 +0100

    Prepress Proof:Reverse the fields <Confirm date,Update date> in the form view

--- __manifest__.py
+++ __manifest__.py
@@ -10,7 +10,7 @@
     'qweb': [],
     'description': False,
     'images': [],
-    'version': '1.0.1.49',
+    'version': '1.0.1.50',
     'category': 'Prepress',
     'demo': [],
     'depends': ['portal',
