0075547aee180fd51872d837164947567889b71c
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2023-01-07 08:11:37 +0100

    Prepress Proof:Auto get the <Weight,Weight Uom> fields from the related product

--- models/prepress_proof.py
+++ models/prepress_proof.py
@@ -6,6 +6,7 @@ from random import randint
 
 QUARANTINE_STATES = ('validated', 'flashed')
 
+
 class PrepressProof(models.Model):
     _name = 'prepress.proof'
     _inherit = ['mail.thread', 'mail.activity.mixin', 'cancel.motif.class']
@@ -29,20 +30,28 @@ class PrepressProof(models.Model):
                                  states={'in_progress': [('readonly', False)]}, readonly=True, copy=False, index=True)
     prepress_type = fields.Many2one('prepress.type', string='Type', states={'in_progress': [('readonly', False)]},
                                     readonly=True)
-    product_height = fields.Float(string='Height',related='product_id.height',states={'in_progress': [('readonly', False)]}, readonly=True,store=True)
-    product_height_uom_id = fields.Many2one('uom.uom',related='product_id.height_uom_id', string="Height Unit of Measure",
-                                            default=lambda self: self.env.ref('uom.product_uom_millimeter'),store=True)
-    product_width = fields.Float(string='Width',related='product_id.width',states={'in_progress': [('readonly', False)]},
-                                 readonly=True,store=True)
-    product_width_uom_id = fields.Many2one('uom.uom',related='product_id.width_uom_id',string="Width unit of Measure",
-                                           default=lambda self: self.env.ref('uom.product_uom_millimeter'),store=True)
-    product_thickness = fields.Float(string='Thickness',related='product_id.thickness', states={'in_progress': [('readonly', False)]}, readonly=True,
+    product_height = fields.Float(string='Height', related='product_id.height',
+                                  states={'in_progress': [('readonly', False)]}, readonly=True, store=True)
+    product_height_uom_id = fields.Many2one('uom.uom', related='product_id.height_uom_id',
+                                            string="Height Unit of Measure",
+                                            default=lambda self: self.env.ref('uom.product_uom_millimeter'), store=True)
+    product_width = fields.Float(string='Width', related='product_id.width',
+                                 states={'in_progress': [('readonly', False)]},
+                                 readonly=True, store=True)
+    product_width_uom_id = fields.Many2one('uom.uom', related='product_id.width_uom_id', string="Width unit of Measure",
+                                           default=lambda self: self.env.ref('uom.product_uom_millimeter'), store=True)
+    product_thickness = fields.Float(string='Thickness', related='product_id.thickness',
+                                     states={'in_progress': [('readonly', False)]}, readonly=True,
                                      store=True)
-    product_thickness_uom_id = fields.Many2one('uom.uom',related='product_id.thickness_uom_id',string="Thickness Unit of Measure",
-                                               default=lambda self: self.env.ref('uom.product_uom_millimeter'),store=True)
-    product_weight = fields.Integer(string='Weight', states={'in_progress': [('readonly', False)]}, readonly=True)
-    product_weight_uom_id = fields.Many2one('uom.uom', string="Weight Unit of Measure",
-                                            default=lambda self: self.env.ref('uom.product_uom_gram'))
+    product_thickness_uom_id = fields.Many2one('uom.uom', related='product_id.thickness_uom_id',
+                                               string="Thickness Unit of Measure",
+                                               default=lambda self: self.env.ref('uom.product_uom_millimeter'),
+                                               store=True)
+    product_gram_weight = fields.Float(string='Weight', related='product_id.gram_weight',
+                                    states={'in_progress': [('readonly', False)]}, readonly=True, store=True)
+    product_gram_weight_uom_id = fields.Many2one('uom.uom', related='product_id.gram_weight_uom_id',
+                                            string="Weight Unit of Measure",
+                                            default=lambda self: self.env.ref('uom.product_uom_gram'), store=True)
     creation_date = fields.Date(string='Creation date', states={'in_progress': [('readonly', False)]}, readonly=True,
                                 default=lambda self: fields.Datetime.now())
     confirm_date = fields.Date(string='Confirm date', states={'in_progress': [('readonly', False)]}, readonly=True)
@@ -62,10 +71,9 @@ class PrepressProof(models.Model):
     state_before_quarantined = fields.Selection([('validated', 'Validated'),
                                                  ('flashed', 'Flashed')])
     quarantined = fields.Boolean(string='Has been quarantined', default=False)
-    flash_line_ids = fields.One2many('prepress.proof.flash.line','prepress_proof_id')
+    flash_line_ids = fields.One2many('prepress.proof.flash.line', 'prepress_proof_id')
     flash_line_ids_count = fields.Integer(compute='_compute_flash_line_ids_count')
     flash_cpt = fields.Integer(string='Flash cpt', default=0)
-
 
     @api.onchange('partner_id')
     def _onchange_partner_id(self):
@@ -76,7 +84,6 @@ class PrepressProof(models.Model):
     def _onchange_product_id(self):
         self.update({
             'prepress_type': self.product_id and self.product_id.prepress_type and self.product_id.prepress_type.id or False})
-
 
     @api.depends('flash_line_ids')
     def _compute_flash_line_ids_count(self):
@@ -105,8 +112,8 @@ class PrepressProof(models.Model):
         self._update_prepress_proof_version()
         self.write({'state': 'validated'})
 
-    def action_flash(self,flash_date,cutting_die,prepress_plate_ctp,prepress_plate_varnish):
-        flash_line = self._prepare_flash_line(flash_date,cutting_die,prepress_plate_ctp,prepress_plate_varnish)
+    def action_flash(self, flash_date, cutting_die, prepress_plate_ctp, prepress_plate_varnish):
+        flash_line = self._prepare_flash_line(flash_date, cutting_die, prepress_plate_ctp, prepress_plate_varnish)
         self.env['prepress.proof.flash.line'].create(flash_line)
         self.incr_flash_cpt()
         if self.state != 'flashed':
@@ -116,14 +123,14 @@ class PrepressProof(models.Model):
         self.ensure_one()
         self.write({'flash_cpt': self.flash_cpt + 1})
 
-    def _prepare_flash_line(self,flash_date,cutting_die,prepress_plate_ctp,prepress_plate_varnish):
+    def _prepare_flash_line(self, flash_date, cutting_die, prepress_plate_ctp, prepress_plate_varnish):
         return {
-            'prepress_proof_id':self.id,
-            'flash_date':flash_date,
-            'cutting_die_id':cutting_die.id,
-            'exposure_nbr':cutting_die.exposure_nbr,
-            'prepress_plate_ctp_id':prepress_plate_ctp and prepress_plate_ctp.id or False,
-            'prepress_plate_varnish_id':prepress_plate_varnish and prepress_plate_varnish.id or False
+            'prepress_proof_id': self.id,
+            'flash_date': flash_date,
+            'cutting_die_id': cutting_die.id,
+            'exposure_nbr': cutting_die.exposure_nbr,
+            'prepress_plate_ctp_id': prepress_plate_ctp and prepress_plate_ctp.id or False,
+            'prepress_plate_varnish_id': prepress_plate_varnish and prepress_plate_varnish.id or False
         }
 
     def _action_flash(self):
@@ -141,7 +148,6 @@ class PrepressProof(models.Model):
     def _action_cancel(self):
         self.write({'state': 'cancel'})
 
-
     def action_quarantine(self):
         self.quarantine_check()
         # we have to register the current state to know how to return
@@ -158,6 +164,7 @@ class PrepressProof(models.Model):
     def action_reset_from_quarantine(self):
         self._check_reset_from_quarantine()
         self._action_reset_from_quarantine()
+
     def _action_reset_from_quarantine(self):
         for each in self:
             each.write({'state': each.state_before_quarantined})
@@ -166,11 +173,11 @@ class PrepressProof(models.Model):
         if any(each.state != 'quarantined' for each in self):
             raise UserError(_("Can not reset from quarantined,you have to check the state!"))
 
-
     def quarantine_check(self):
         for each in self:
             if each.state not in QUARANTINE_STATES:
-                raise UserError(_("Can not put the Prepress Proof %s in quarantine,you have to check the state!") % each.name)
+                raise UserError(
+                    _("Can not put the Prepress Proof %s in quarantine,you have to check the state!") % each.name)
 
     def _update_prepress_proof_version(self):
         for each in self:
@@ -205,7 +212,7 @@ class PrepressProof(models.Model):
     def unlink(self):
         if any(prepress_proof.state != 'in_progress' for prepress_proof in self):
             raise ValidationError(_("Only in progress Prepress Proofs can be removed!"))
-        return super(PrepressProof,self).unlink()
+        return super(PrepressProof, self).unlink()
 
     # FIXME:This 2 checks must be tested with > 100000 Prepress Proof for evaluating it's performance
     @api.model
@@ -216,13 +223,13 @@ class PrepressProof(models.Model):
             raise ValidationError(_("Only one in progress Prepress Proof is authorised by product!"))
 
     def _check_validated_prepress_proofs(self):
-        domain = [('state', 'in', ('validated', 'flashed','quarantined')), ('product_id', 'in', self.mapped("product_id").ids)]
+        domain = [('state', 'in', ('validated', 'flashed', 'quarantined')),
+                  ('product_id', 'in', self.mapped("product_id").ids)]
         if self.search_count(domain) > 0:
             raise ValidationError(_("Only one Validated/Flashed Prepress Proof is authorised by product!"))
         for each in self:
             if not each.confirm_date:
                 raise ValidationError(_("Confirm date is required!"))
-
 
     def action_flash_wizard(self):
         ''' Open the prepress.proof.flash wizard to flash the current Prepress Proof.
@@ -232,9 +239,9 @@ class PrepressProof(models.Model):
         # we have to exclude already select Plate CTPs from being select again
         excluded_plate_ctp_ids = self.flash_line_ids.mapped("prepress_plate_ctp_id").ids
         prepress_proof_flash_wizard_id = self.env['prepress.proof.flash'].create({
-            'product_id':self.product_id.id,
-            'partner_id':self.partner_id.id,
-            'excluded_plate_ctp_ids':[(6, 0, excluded_plate_ctp_ids)]
+            'product_id': self.product_id.id,
+            'partner_id': self.partner_id.id,
+            'excluded_plate_ctp_ids': [(6, 0, excluded_plate_ctp_ids)]
         })
         return {
             'name': _('Flash Prepress proof'),
@@ -244,7 +251,7 @@ class PrepressProof(models.Model):
                 'active_model': 'prepress.proof',
                 'active_ids': self.ids,
             },
-            'res_id':prepress_proof_flash_wizard_id.id,
+            'res_id': prepress_proof_flash_wizard_id.id,
             'target': 'new',
             'type': 'ir.actions.act_window',
         }
@@ -255,11 +262,11 @@ class PrepressProofFlashLine(models.Model):
 
     # fields
     prepress_proof_id = fields.Many2one('prepress.proof', ondelete='cascade')
-    cutting_die_id = fields.Many2one('prepress.cutting.die', string="Cutting Die",required=True)
+    cutting_die_id = fields.Many2one('prepress.cutting.die', string="Cutting Die", required=True)
     prepress_plate_ctp_id = fields.Many2one('prepress.plate', string="CTP Plate")
     prepress_plate_varnish_id = fields.Many2one('prepress.plate', string="Varnish Plate")
     is_default = fields.Boolean(string="Default", default=False)
-    exposure_nbr = fields.Integer('Exposure Nbr',related='cutting_die_id.exposure_nbr',store=True)
+    exposure_nbr = fields.Integer('Exposure Nbr', related='cutting_die_id.exposure_nbr', store=True)
 
     company_id = fields.Many2one('res.company', 'Company', required=True, default=lambda s: s.env.company.id,
                                  index=True)
@@ -267,9 +274,10 @@ class PrepressProofFlashLine(models.Model):
 
     def unlink(self):
         for each in self:
-            if each.prepress_proof_id and each.prepress_proof_id.state in ('validated','flashed'):
+            if each.prepress_proof_id and each.prepress_proof_id.state in ('validated', 'flashed'):
                 raise ValidationError(_("Can not remove flash line of Validated/Flashed Prepress Proof"))
-        return super(PrepressProofFlashLine,self).unlink()
+        return super(PrepressProofFlashLine, self).unlink()
+
 
 class PrepressProofColor(models.Model):
     _name = 'prepress.proof.color'
@@ -279,6 +287,3 @@ class PrepressProofColor(models.Model):
     color_id = fields.Many2one('product.product', string='Reference', required=True)
     color_code = fields.Char(string='Color', required=True, store=True)
     rate = fields.Float(string='Rate (%)')
-
-
-
