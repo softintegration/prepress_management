fef52811eb42c245a4d7b2cdca9612552a8316f5
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2023-01-14 16:28:04 +0100

    Prepress Proof:Add Grouping By <Status>

--- __manifest__.py
+++ __manifest__.py
@@ -10,7 +10,7 @@
     'qweb': [],
     'description': False,
     'images': [],
-    'version': '1.0.1.66',
+    'version': '1.0.1.67',
     'category': 'Prepress',
     'demo': [],
     'depends': ['portal',
