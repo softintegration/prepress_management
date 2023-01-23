b7cfc8818d8cfba8f7efaf2ae55d961d95be9456
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2023-01-24 00:28:28 +0100

    Prepress proof:Fix the frensh translation of status <Quarantined>

--- __manifest__.py
+++ __manifest__.py
@@ -10,7 +10,7 @@
     'qweb': [],
     'description': False,
     'images': [],
-    'version': '1.0.1.72',
+    'version': '1.0.1.73',
     'category': 'Prepress',
     'demo': [],
     'depends': ['portal',
