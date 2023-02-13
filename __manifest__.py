4f073c361fed93d19c1cf93a4d96ce0e8a37f587
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2023-02-13 19:46:35 +0100

    Product:Correct the frensh translation of field <Varnish type>

--- __manifest__.py
+++ __manifest__.py
@@ -10,7 +10,7 @@
     'qweb': [],
     'description': False,
     'images': [],
-    'version': '1.0.1.81',
+    'version': '1.0.1.82',
     'category': 'Prepress',
     'demo': [],
     'depends': ['portal',
