1e60d1c326d891a047eb79232ddf9fdb1686179b
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2023-05-16 15:29:40 +0100

    Plate:Add the field «Tags» in the tree view

--- __manifest__.py
+++ __manifest__.py
@@ -10,7 +10,7 @@
     'qweb': [],
     'description': False,
     'images': [],
-    'version': '1.0.1.115',
+    'version': '1.0.1.116',
     'category': 'Prepress',
     'demo': [],
     'depends': ['portal',
