17b6a899f51f42a813b164dbd7c278aeaa76bb8d
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2023-05-16 15:38:12 +0100

    Plate:Replace the field «Cutting die» before the field «Mounting height» in the tree view

--- __manifest__.py
+++ __manifest__.py
@@ -10,7 +10,7 @@
     'qweb': [],
     'description': False,
     'images': [],
-    'version': '1.0.1.116',
+    'version': '1.0.1.117',
     'category': 'Prepress',
     'demo': [],
     'depends': ['portal',
