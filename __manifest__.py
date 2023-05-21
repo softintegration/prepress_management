fc6e99a887f850ff5756a005fbe31ec5e0066804
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2023-05-21 04:44:43 +0100

    Plate:Move the field «Exposure Nbr» after the field «Mounting width» in the tree view

--- __manifest__.py
+++ __manifest__.py
@@ -10,7 +10,7 @@
     'qweb': [],
     'description': False,
     'images': [],
-    'version': '1.0.1.117',
+    'version': '1.0.1.118',
     'category': 'Prepress',
     'demo': [],
     'depends': ['portal',
