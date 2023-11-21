22bf35a580604eb87b70e03879c51a9da76655d9
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2023-11-21 11:55:19 +0100

    Prepress proof:Add «Send by Email» operation

--- models/prepress_proof.py
+++ models/prepress_proof.py
@@ -92,6 +92,7 @@ class PrepressProof(models.Model):
     flash_cpt = fields.Integer(string='Flash cpt', default=0)
     prepress_plates_ids_count = fields.Integer(compute='_compute_prepress_plates_ids_count')
     customer_signatures = fields.One2many('prepress.proof.customer.signature', 'prepress_proof_id')
+    is_sent = fields.Boolean(string='Already sent by mail',default=False)
     locked = fields.Boolean(string='Locked', help="If the prepress proof is locked,no field can be edited",
                             default=False,track_visibility=True)
 
@@ -429,6 +430,43 @@ class PrepressProof(models.Model):
             'target': 'new',
             'type': 'ir.actions.act_window',
         }
+
+    def action_prepress_proof_send(self):
+        self.ensure_one()
+        # FIXME:in the template email_template_prepress_proof, we have used the report prepress_proof_report_lbe.action_report_prepress_proof
+        # FIXME (suite): this report has to be changed by the good one because the parent module of this report is temporary
+
+        template_id = self.env['ir.model.data']._xmlid_to_res_id('prepress_management.email_template_prepress_proof', raise_if_not_found=False)
+        lang = self.env.context.get('lang')
+        template = self.env['mail.template'].browse(template_id)
+        if template.lang:
+            lang = template._render_lang(self.ids)[self.id]
+        ctx = {
+            'default_model': self._name,
+            'default_res_id': self.ids[0],
+            'default_use_template': bool(template_id),
+            'default_template_id': template_id,
+            'default_composition_mode': 'comment',
+            'mark_so_as_sent': True,
+            'custom_layout': "mail.mail_notification_paynow",
+            'proforma': self.env.context.get('proforma', False),
+            'force_email': True,
+            'model_description': _('Prepress proof'),
+        }
+        return {
+            'type': 'ir.actions.act_window',
+            'view_mode': 'form',
+            'res_model': 'mail.compose.message',
+            'views': [(False, 'form')],
+            'view_id': False,
+            'target': 'new',
+            'context': ctx,
+        }
+
+    @api.returns('mail.message', lambda value: value.id)
+    def message_post(self, **kwargs):
+        self.write({'is_sent': True})
+        return super(PrepressProof, self).message_post(**kwargs)
 
 
 class PrepressProofFlashLine(models.Model):
