98b51fa42a4eb9954b15523bebf5015aa0d3fb13
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2023-12-24 08:58:34 +0100

    [FIX] Prepress proof:Set the «Email from» to the session user

--- __manifest__.py
+++ __manifest__.py
@@ -10,7 +10,7 @@
     'qweb': [],
     'description': False,
     'images': [],
-    'version': '1.0.1.152',
+    'version': '1.0.1.153',
     'category': 'Prepress',
     'demo': [],
     'depends': ['portal',
