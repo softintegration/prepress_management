b1619c43cdd984aae99c965353d36378ffa64f28
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2024-02-04 12:16:59 +0100

    [FIX] Cutting die:Fix the readonly attribute in «Draft» state of fields «Customers» and «Equipments»

--- __manifest__.py
+++ __manifest__.py
@@ -10,7 +10,7 @@
     'qweb': [],
     'description': False,
     'images': [],
-    'version': '1.0.1.153',
+    'version': '1.0.1.154',
     'category': 'Prepress',
     'demo': [],
     'depends': ['portal',
